a19ca8a165eeefe36fc3872e8f3b81de9ac6f85f
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-03-27 20:40:54 +0300

    improved code by writing standards

--- src/base-components/Loading/Loading.jsx
+++ src/base-components/Loading/Loading.jsx
@@ -6,7 +6,7 @@ import './Loading.scss';
 export default function Loading() {
   return (
     <div className="loading-content-wrapper">
-      <TailSpin stroke="black" speed={0.8} height={'10em'} width={'10em'} />
+      <TailSpin stroke="black" speed={0.8} height="10em" width="10em" />
       <h2>Loading data, please wait...</h2>
     </div>
   );
