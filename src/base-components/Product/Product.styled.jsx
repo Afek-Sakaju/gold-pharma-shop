cab8f44bc4dead9d099dc2a4077590648303139a
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-10-17 17:25:43 +0300

    redesigned the product buttons and the product container

--- src/base-components/Product/Product.styled.jsx
+++ src/base-components/Product/Product.styled.jsx
@@ -5,26 +5,25 @@ export const ProductContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 200px;
+  height: 260px;
   flex: 1 1 auto;
   border: 1px solid rgba(black, 0.2);
-  border-radius: 10px;
+  border-radius: 74% 26% 68% 32% / 26% 66% 34% 74%;
   background-color: white;
-  box-shadow: 5px 5px 10px -1px rgba(black, 0.15);
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
   transition: all 0.2s ease;
-  opacity: 0.95;
+  user-select: none;
   cursor: pointer;
-
-  &:hover {
-    border: 1px solid rgba(black, 0.4);
-    box-shadow: 5px 5px 12px 2px rgba(black, 0.15);
-    transform: translateY(-1px);
-    opacity: 1;
-  }
+  overflow: hidden;
 `;
 
 export const ProductContentContainer = styled.div`
-  padding: 0 0.6em 0.6em 0.6em;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 0.7em 0.8em 0.7em;
 `;
 
 export const ButtonsContainer = styled.div`
@@ -42,7 +41,7 @@ export const ProductName = styled.p`
 export const ProductPrice = styled.p`
   font-size: 1em;
   user-select: none;
-  margin: 10px 0;
+  margin: 8.5px 0;
 `;
 
 export const ProductImage = styled.img`
@@ -53,10 +52,27 @@ export const ProductImage = styled.img`
 `;
 
 export const AddProductButton = styled(Button)`
-  height: 40px;
+  justify-content: space-between;
+  gap: 10px;
+  padding: 8px;
+  color: #13690a;
+  background-color: #fafafa;
+  border: 1px solid #13690ac8;
+  border-radius: 74% 26% 68% 32% / 24% 59% 41% 76%;
+  box-shadow: 0px 2.5px 5px rgba(19, 105, 10, 0.2),
+    0px 2.5px 5px rgba(19, 105, 10, 0.1);
+  font-size: 1.1em;
 `;
 
 export const RemoveProductButton = styled(Button)`
+  max-width: 49%;
   height: 40px;
+  justify-content: space-between;
   background-color: #fe5a3de3;
+  font-size: 0.9em;
+  border: 1px solid black;
+
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px 1px rgba(black, 0.15);
+  white-space: pre;
 `;
