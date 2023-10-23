d475d05d5c5fdcd721f5250642b36a91df6ca4c0
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-10-24 01:41:57 +0300

    when reduced amount of products, noticed that some style changes are crucial to prevent UI bugs

--- src/components/ProductList/ProductList.styled.jsx
+++ src/components/ProductList/ProductList.styled.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
 export const ProductListContainer = styled.div`
+  height: 100%;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
