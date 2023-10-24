cdb0c4c6640c4d48ad54cc88e941b5ce7ac44d3d
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-10-24 03:17:14 +0300

    removed unused const

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,20 +1,2 @@
-// This mock list used only for storybook
-export const MOCK_PRODUCTS = [
-  {
-    id: '81ff8102-d0dc-4ce1-b944-3a0b4c787c1b',
-    productName: 'Beef tallow',
-    price: 100,
-  },
-  {
-    id: 'a707701a-3570-482a-8ec3-bb1fa888bcb7',
-    productName: 'Cucumber',
-    price: 4,
-  },
-  {
-    id: '8ab6e340-cbec-4f71-ab41-2df4e07ce6cf',
-    productName: 'Strawberries',
-    price: 15,
-  },
-];
-
+// eslint-disable-next-line import/prefer-default-export
 export const SERVER_URL = process.env.REACT_APP_DB_URL;
