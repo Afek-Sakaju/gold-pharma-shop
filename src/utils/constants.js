0e7ea22cbadd1486c7dd7b574e26f9f20c6bd67f
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-10-24 10:26:19 +0300

    removed unnecessary comment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/prefer-default-export
 export const SERVER_URL = process.env.REACT_APP_DB_URL;
 
 export const LOGO_SRC = '/Afek-Sakaju/grocery-store/shop-logo.png';
